0e06c9a8cca071471a203e21c140778aed214441
Romerito Barbosa <romeritobarbosa30@gmail.com>
2022-06-02 00:44:06 -0300

    Projeto alterado para tomcat 9

--- src/main/java/br/com/gestaodentista/modelo/servlets/ServletConsultaController.java
+++ src/main/java/br/com/gestaodentista/modelo/servlets/ServletConsultaController.java
@@ -5,17 +5,18 @@ import java.sql.Date;
 import java.text.SimpleDateFormat;
 import java.util.List;
 
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
 import com.fasterxml.jackson.databind.ObjectMapper;
 
 import br.com.gestaodentista.modelo.dao.DAOConsultaRepository;
 import br.com.gestaodentista.modelo.entidade.Consulta;
-import br.com.gestaodentista.modelo.entidade.Procedimento;
-import jakarta.servlet.RequestDispatcher;
-import jakarta.servlet.ServletException;
-import jakarta.servlet.annotation.WebServlet;
-import jakarta.servlet.http.HttpServlet;
-import jakarta.servlet.http.HttpServletRequest;
-import jakarta.servlet.http.HttpServletResponse;
+
 
 @WebServlet("/ServletConsultaController")
 public class ServletConsultaController extends HttpServlet {
